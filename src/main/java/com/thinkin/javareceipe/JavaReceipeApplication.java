cdf443d96b9b52071cc7c3eff042391d2de9f820
Ashish Tyagi <ashishtyagi10@gmail.com>
2017-09-30 22:57:18 -0400

    first commit

---
+++ src/main/java/com/thinkin/javareceipe/JavaReceipeApplication.java
@@ -0,0 +1,12 @@
+package com.thinkin.javareceipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReceipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReceipeApplication.class, args);
+	}
+}
